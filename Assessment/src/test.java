7d64b7ce7142774ef2cc7b9e57aaa4434e9be9bd
Jennifer Cano <jcano2@oxy.edu>
2021-01-24 22:44:05 -0800

    Update test.java

--- Assessment/src/test.java
+++ Assessment/src/test.java
@@ -1,31 +1,26 @@
 public class test extends assessmentTest{
 	
 	public static void main(String[] args){
-				
-		Robot robot1 = new Robot(1);
+
+		//First Customer who orders 2 orders of French fries
 		
-		Customer customer1 = new Customer(1, 0);
-		
-		Orders order = new Orders(customer1);
-		Chef frenchfries = new Chef(2);
+		Robot robot1 = new Robot(1);  //Creates the robot that will be in charge of your order 
+		Customer customer1 = new Customer(1, 0);  //First parameter=customerId...the 2nd value is their previous balance but this is always 0
+		Orders order = new Orders(customer1); 
+		Chef frenchfries = new Chef(2);  //Parameter = number of French fries
 		order.addItem(frenchfries);
-		
 		robot1.takeOrder(order);
-		
 		robot1.prepare();
 		
 		
+		//Second Customer who orders 5 orders of French fries
 		
 		Robot robot2 = new Robot(2);
-		
-		Customer customer2 = new Customer(1, 0);
-		
+		Customer customer2 = new Customer(2, 0);
 		Orders order2 = new Orders(customer2);
 		Chef frenchfries2 = new Chef(5);
 		order2.addItem(frenchfries2);
-		
 		robot2.takeOrder(order2);
-		
 		robot2.prepare();
 		
 	}
